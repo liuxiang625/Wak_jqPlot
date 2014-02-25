4e8fdc3b85f867f209ab434b0dde3fa4b548aebb
Xiang Liu <liuxiang625@gmail.com>
2014-02-24 16:13:15 -0800

    Initial Commit. Wak_jqPlot with basic Line chart and Pie chart available. More renderer and chart customization in development

---
+++ CustomWidget/CustomWidget/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+	chartDataSeries = []
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		chartDataSeries = [{'xValue':1,'yValue':2}];
+		sources.chartDataSeries.sync();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
